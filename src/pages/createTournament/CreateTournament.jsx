ef34bbbf5d4df039b313af708a21f992be39c405
macdrien <adrien.bouyssou@gmail.com>
2023-07-06 11:55:52 +0200

    ADD Teams names inputs

--- src/pages/createTournament/CreateTournament.jsx
+++ src/pages/createTournament/CreateTournament.jsx
@@ -1,21 +1,30 @@
 import { useState } from "react";
 import "./CreateTournament.css";
 import TeamsCountSelector from "./TeamCountSelector";
+import TeamsNames from "./TeamsNames";
 
 const CreateTournament = () => {
   const [state, setState] = useState({
     options: [2, 4, 8, 16],
-    checkedValue: 4,
+    numberOfTeams: 4,
+    teams: Array(16).fill('', 0),
   });
 
-  const changeSelection = newValue => {
-    setState({...state, checkedValue: newValue});
+  const onTeamsCountChange = newValue => {
+    setState({...state, numberOfTeams: newValue});
+  }
+
+  const onTeamNameChange = (index, newName) => {
+    const teams = state.teams;
+    teams.splice(index, 1, newName);
+    setState({...state, teams});
   }
 
   return (
     <section className="createTournament">
       <form className="tournamentCreation">
-        <TeamsCountSelector options={state.options} checkedValue={state.checkedValue} changeSelection={changeSelection}/>
+        <TeamsCountSelector options={state.options} checkedValue={state.numberOfTeams} changeSelection={onTeamsCountChange}/>
+        <TeamsNames teams={state.teams} onChange={onTeamNameChange} teamsCount={state.numberOfTeams}/>
       </form>
     </section>
   );
---
+++ src/pages/createTournament/TeamsNames.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const TeamsNames = (props) => {
+    const { teams, onChange, teamsCount } = props;
+
+    return <div className="teamsNames">
+        <p>Teams names</p>
+        { teams.slice(0, teamsCount).map((team, index) => 
+            <input
+                key={index}
+                type="text"
+                value={team}
+                onChange={event => onChange(index, event.target.value)}
+                placeholder={['Team name', index + 1].join(' ')}
+            />)}
+    </div>;
+}
+
+TeamsNames.propTypes = {
+    teams: PropTypes.arrayOf(PropTypes.string).isRequired,
+    onChange: PropTypes.func.isRequired,
+    teamsCount: PropTypes.number.isRequired,
+}
+
+export default TeamsNames;
