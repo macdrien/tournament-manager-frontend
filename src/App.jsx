fac6df190fbb81b7dee783b86709137a16d214c8
macdrien <adrien.bouyssou@gmail.com>
2023-07-06 10:02:30 +0200

    ADD Initialize create tournament page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import { Header } from './pages';
+import CreateTournament from './pages/createTournament/CreateTournament';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Header />
-    </>
+      <div className='content'>
+        <CreateTournament />
+      </div>
+    </div>
   );
 }
 
---
+++ src/pages/createTournament/CreateTournament.jsx
@@ -0,0 +1,5 @@
+const CreateTournament = () => {
+    return <section>This is a test</section>
+}
+
+export default CreateTournament;
