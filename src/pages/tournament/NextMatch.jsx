9c1cb16285f4e03cb1d8cd343ca5d1d195c566e3
macdrien <adrien.bouyssou@gmail.com>
2023-07-13 23:14:55 +0200

    ADD Next match

--- src/pages/tournament/NextMatch.jsx
+++ src/pages/tournament/NextMatch.jsx
@@ -1,15 +1,33 @@
+import PropTypes from "prop-types";
+
 const NextMatch = (props) => {
   const { match } = props;
   const { teams } = match;
-  console.log(match);
 
   return (
     <div className="nextMatch">
       <div className="nextMatchTeam">{teams[0]}</div>
-      VS
+      <input
+        type="number"
+        className="nextMatchScoreInput"
+        value={match.result && match.result[0] ? match.result[0] : null}
+      />
+      <div className="nextMatchVs">VS</div>
+      <input
+        type="number"
+        className="nextMatchScoreInput"
+        value={match.result && match.result[0] ? match.result[1] : null}
+      />
       <div className="nextMatchTeam">{teams[1]}</div>
     </div>
   );
+};
+
+NextMatch.propTypes = {
+  match: PropTypes.shape({
+    teams: PropTypes.arrayOf(PropTypes.string),
+    result: PropTypes.arrayOf(PropTypes.number),
+  }),
 };
 
 export default NextMatch;
