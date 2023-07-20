59039ba4df37b60045faef721895b6bc3f7a40d8
macdrien <adrien.bouyssou@gmail.com>
2023-07-20 12:59:27 +0200

    ADD Display scores in brackets for matches done

--- src/pages/tournament/brackets/Match.jsx
+++ src/pages/tournament/brackets/Match.jsx
@@ -7,12 +7,28 @@ const Match = function (props) {
   return (
     <>
       <div className="match">
-        <div className="teamName">{teams[0]}</div>
+        <div className="teamSection">
+          <div>{teams[0]}</div>
+          {match.matchDone ?
+              <div className="teamScore">
+                {match.result[0] > match.result[1] ? <div>&#9819;</div> : ''}
+                {match.result[0]}
+              </div>
+            : ''}
+          </div>
         <div className="matchSeparator"></div>
         <button className="vs" disabled={!(teams[0] && teams[1])}>
           VS
         </button>
-        <div className="teamName">{teams[1]}</div>
+        <div className="teamSection">
+          <div>{teams[1]}</div>
+          {match.matchDone ?
+              <div className="teamScore">
+                {match.result[1] > match.result[0] ? <div>&#9819;</div> : ''}
+                {match.result[1]}
+              </div>
+            : ''}
+        </div>
       </div>
     </>
   );
@@ -22,6 +38,7 @@ Match.propTypes = {
   match: PropTypes.shape({
     teams: PropTypes.arrayOf(PropTypes.string),
     result: PropTypes.arrayOf(PropTypes.number),
+    matchDone: PropTypes.bool,
   }),
 };
 
