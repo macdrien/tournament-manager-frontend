99a6e161c50f915326ae3a9cabf7441cce70893a
macdrien <adrien.bouyssou@gmail.com>
2023-07-21 20:23:03 +0200

    FIX Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://macdrien.github.io/tournament-manager',
+  base: "/tournament-manager/",
   plugins: [react()],
-})
+});
