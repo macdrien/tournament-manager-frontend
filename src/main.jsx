6ec30aeca5706a2fc0f2112156c4ea0b70b27ab9
macdrien <adrien.bouyssou@gmail.com>
2023-07-21 22:35:21 +0200

    FIX Routing with github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import CreateTournament, {
@@ -8,29 +8,24 @@ import CreateTournament, {
 } from "./pages/createTournament/CreateTournament";
 import Tournament, { loaderTournament } from "./pages/tournament/Tournament";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          path: "create",
-          element: <CreateTournament />,
-          action: actionCreate,
-        },
-        {
-          path: "tournament",
-          element: <Tournament />,
-          loader: loaderTournament,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: "/tournament-manager",
-  }
-);
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "create",
+        element: <CreateTournament />,
+        action: actionCreate,
+      },
+      {
+        path: "tournament",
+        element: <Tournament />,
+        loader: loaderTournament,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
