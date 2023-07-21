545e9e9557d7f4d592917d5bdc57477b8908de9a
macdrien <adrien.bouyssou@gmail.com>
2023-07-21 22:50:02 +0200

    ADD redirection message on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from "./pages";
 import "./App.css";
-import { Outlet, useLocation } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 function App() {
   const tournamentName = "Tournament";
@@ -18,7 +18,27 @@ function App() {
     <div className="app">
       <Header title={pageTitle()} />
       <div className="content">
-        <Outlet />
+        {location.pathname === "/" ? (
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              textAlign: "center",
+              gap: "1em",
+            }}
+          >
+            <span>This page is currently empty.</span>
+            <span>
+              You can click on the button bellow to create a tournament
+            </span>
+            <Link to="/create">
+              <button>Create</button>
+            </Link>
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </div>
     </div>
   );
