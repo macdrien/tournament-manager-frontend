cf7ac51742e6a0b901ab56028c1ac7bf53028845
macdrien <adrien.bouyssou@gmail.com>
2023-07-21 15:33:30 +0200

    ADD basepath

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://macdrien.github.io/tournament-manager',
   plugins: [react()],
 })
