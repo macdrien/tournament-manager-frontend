1ce7765eef439da959190bf525a39462bf61411c
macdrien <adrien.bouyssou@gmail.com>
2023-08-04 11:24:19 +0200

    ADD Theme switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useState } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import { Header } from "./pages";
 import "./App.css";
-import { Outlet, useLocation } from "react-router-dom";
 
-function App() {
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
   const tournamentName = "Tournoi";
   const location = useLocation();
 
@@ -14,9 +16,13 @@ function App() {
     }
   };
 
+  const onThemeChange = event => {
+    setDarkTheme(event.target.checked);
+  }
+
   return (
-    <div className="app">
-      <Header title={pageTitle()} />
+    <div className="app" data-theme={darkTheme ? 'dark' : 'light'}>
+      <Header title={pageTitle()} onThemeChange={onThemeChange} darkTheme={darkTheme}/>
       <div className="content">
         <Outlet />
       </div>
--- src/pages/header/Header.jsx
+++ src/pages/header/Header.jsx
@@ -2,17 +2,25 @@ import PropTypes from "prop-types";
 import "./Header.css";
 
 const Header = (props) => {
-  const { title } = props;
+  const { title, onThemeChange, darkTheme } = props;
   return (
     <header>
       <h1>Tournament manager</h1>
       <h2>{title}</h2>
+      <div className='toggleTheme'>
+        <label>
+          <input type='checkbox' value={darkTheme} onChange={onThemeChange}/>
+          <span className='slider'></span>
+        </label>
+      </div>
     </header>
   );
 };
 
 Header.propTypes = {
   title: PropTypes.string,
+  onThemeChange: PropTypes.func.isRequired,
+  darkTheme: PropTypes.bool.isRequired,
 };
 
 export default Header;
