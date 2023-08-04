cf5ccb32b3bec6934d360b672edac84aa4f5c92d
macdrien <adrien.bouyssou@gmail.com>
2023-08-04 11:29:04 +0200

    DEL index.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createHashRouter, redirect, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
 import CreateTournament, {
   actionCreate,
 } from "./pages/createTournament/CreateTournament";
