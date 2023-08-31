f648967fff7afb41429ccb94c3a06e9a292a238a
macdrien <adrien.bouyssou@gmail.com>
2023-08-31 22:05:00 +0200

    ADD Display duplicates player names

--- src/pages/createTournament/PlayersNames.jsx
+++ src/pages/createTournament/PlayersNames.jsx
@@ -1,7 +1,19 @@
+import {useState, useEffect} from 'react';
 import PropTypes from "prop-types";
+
+import { findDuplicates } from '../../utils.js';
 
 const PlayersNames = (props) => {
   const { players, onChange, playersCount } = props;
+  const [state, setState] = useState({ duplicates: [] });
+
+  const analysePlayers = () => {
+    setState({ duplicates: findDuplicates(players) });
+  }
+
+  useEffect(() => {
+    analysePlayers();
+  }, []);
 
   return (
     <div className="playersNames">
@@ -10,10 +22,11 @@ const PlayersNames = (props) => {
         {players.slice(0, playersCount).map((player, index) => (
           <input
             key={index}
+            className={`${state.duplicates.length && state.duplicates.includes(player) ? 'error' : ''}`}
             type="text"
             name={`players[${index}]`}
             value={player}
-            onChange={(event) => onChange(index, event.target.value)}
+            onChange={(event) => { onChange(index, event.target.value); analysePlayers(); }}
             placeholder={["Joueur ", index + 1].join(" ")}
           />
         ))}
